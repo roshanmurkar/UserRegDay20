eb5665cc59f9b06a4b3f9147eeaf21492b64681c
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-07-31 11:35:44 +0530

    validation for first name

---
+++ main/FirstNAmeValidation.java
@@ -0,0 +1,22 @@
+package main;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FirstNAmeValidation {
+	public void firstNameValidate(String str) {
+		int flag = 0;
+		Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,10}$");
+		Matcher matcher = pattern.matcher(str);
+		while (matcher.find()) {
+			flag = 1;
+		}
+		if (flag == 1) {
+			System.out.println("Valid First Name");
+		} else {
+			System.out.println("invalid First Name");
+		}
+
+	}
+}
--- main/Main.java
+++ main/Main.java
@@ -1,5 +1,9 @@
 package main;
 
 public class Main {
-
+	public static void main(String[] args) {
+	
+		FirstNAmeValidation firstNAmeValidation=new FirstNAmeValidation();
+		firstNAmeValidation.firstNameValidate("Roshan");
+	}
 }
